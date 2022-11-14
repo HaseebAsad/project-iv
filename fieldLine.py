d8f7406d0dca6ab77a9c25c518886efc3aaaa99d
HaseebAsad <89456132+HaseebAsad@users.noreply.github.com>
2022-11-14 16:57:42 +0000

    Update fieldLine.py

--- fieldLine.py
+++ fieldLine.py
@@ -14,24 +14,26 @@ Worth noting that the matplotlib function will expect the inputs to be in Cartes
 Let our example be for a "single source". Priest 03
 """
 """
-Below is a simple harmonic oscillator
+Example is the ABC magnetic field ()
 """
 
-def fieldLine(B, t, k, m):
-    x, y = B
-    dBds = [-(k/m)*y, x]
+def fieldLine(M, t, A, B, C):
+    x, y, z = M
+    dBds = [A*np.sin(z)+C*np.cos(y), B*np.sin(x)+A*np.cos(z), C*np.sin(y)+B*np.cos(x)]
     return dBds
 
-k=0.5
-m=1
+A = 1
+B = 1
+C = 1
 
-y0 = [np.pi - 0.1, 0.0]
+y0 = [np.pi, -np.pi, 0.0]
 t = np.linspace(0, 10, 101)
 
-sol = integrate.odeint(fieldLine, y0, t, args=(m, k)) #Will give an array of numerical solutions to the ODE.
+sol = integrate.odeint(fieldLine, y0, t, args=(A, B, C)) #Will give an array of numerical solutions to the ODE.
 #Plot the solution to the ODE
-plt.plot(t, sol[:, 0], 'b', label='theta(t)')
-plt.plot(t, sol[:, 1], 'g', label='omega(t)')
+plt.plot(t, sol[:, 0], 'b', label='x(t)')
+plt.plot(t, sol[:, 1], 'g', label='y(t)')
+plt.plot(t, sol[:, 2], 'r', label='z(t)')
 plt.legend(loc='best')
 plt.xlabel('t')
 plt.grid()
