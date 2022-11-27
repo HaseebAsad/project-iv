98afd5a855047c49842e42d9083d977a2ba291ad
HaseebAsad <haseeb.asad@gmail.com>
2022-11-27 16:17:21 +0000

    Update fieldLine.py
    Fixed initial conditions around null points so that we have a more reasonable plot.

--- fieldLine.py
+++ fieldLine.py
@@ -27,17 +27,35 @@ def fieldLine(M, t, C):
 
 C = 1
 
-t = np.linspace(-5, 5, 100)
+t = np.linspace(0, 2, 100)
+t2 = np.linspace(0,-2,100)
 
 #Plot of the field lines (in 3D!)
 
 ax = plt.axes(projection='3d')
-N = 10 #Number of (equally spaced) ICs in the interval
+N = 20 #Number of (equally spaced) ICs in the interval
 l = 5 #half-length of volume of cube to plot in (symmetric around axis)
+
+"""
+Creating a 'box' of initial conditions around the null point of side lengths 0.1.
+"""
 for i in range (N):
-    B0 = [-l + i*(2*l)/N, 0, -l + i*(2*l)/N] #equally spaced intervals in cube
+    B0 = [-l + i*(2*l)/N, 0.1, 0] #equally spaced intervals in square/circle around (0,0)
     sol = integrate.odeint(fieldLine, B0, t, args=(C,)) #for our basic example this keeps outputting x = y?
     ax.plot3D(sol[:, 0],sol[:, 1],sol[:, 2])
+for i in range (N):
+    B0 = [0.1, -l + i*(2*l)/N, 0] #equally spaced intervals in square/circle around (0,0)
+    sol = integrate.odeint(fieldLine, B0, t, args=(C,)) #for our basic example this keeps outputting x = y?
+    ax.plot3D(sol[:, 0],sol[:, 1],sol[:, 2])
+for i in range (N):
+    B0 = [-l + i*(2*l)/N, 0.1, 0] #TODO: starting positions around sphere or random.random() but in full range (less preferred)
+    sol = integrate.odeint(fieldLine, B0, t2, args=(C,)) #for our basic example this keeps outputting x = y?
+    ax.plot3D(sol[:, 0],sol[:, 1],sol[:, 2])
+for i in range (N):
+    B0 = [0.1, -l + i*(2*l)/N, 0] #equally spaced intervals in square/circle around (0,0)
+    sol = integrate.odeint(fieldLine, B0, t2, args=(C,)) #for our basic example this keeps outputting x = y?
+    ax.plot3D(sol[:, 0],sol[:, 1],sol[:, 2])
+
 ax.set_zlim(-5,5)
 ax.set_xlim(-5,5)
 ax.set_ylim(-5,5)
