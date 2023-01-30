0c191e68e42aae5b48e30943b0ccccfaaf1534b8
HaseebAsad <haseeb.asad@gmail.com>
2023-01-30 22:05:22 +0000

    Patch 30.01

--- magnetic_particle_model.py
+++ magnetic_particle_model.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 import os
 from scipy.integrate import odeint
+import itertools
 """
 Using our code previously built, this code will be a construction of small N particles interacting, modelling in a random walk motion.
 We implement periodic boundary conditions: if particles leave a given domain, it will mirror itself and return back inside the "box" 
@@ -10,7 +11,7 @@ by flipping the coordinates (may be easier to simply implement taking away the l
 """
 
 # Constants
-n_particles = 5 # Number of particles
+n_particles = 4 # Number of particles
 velocity_scale = 3 # Determines the magnitude of each random walk
 box_length = 5
 n_points  = 100
@@ -24,7 +25,7 @@ polarity = np.zeros(n_particles) # Polarity encodes the charge too.
 colors = np.zeros(n_particles, dtype=str)
 
 # Set initial positions, and polarities of particles
-polarity_values = np.arange(-10,10,1) # Possible polarity values. This allows us to randomise polarities such that they may sum to zero.
+polarity_values = np.array(list(itertools.chain(*[range(-10, 0), range(1, 10+1)]))) # Possible polarity values. This allows us to randomise polarities such that they may sum to zero.
 
 for i in range(n_particles):
     x[i] = np.random.uniform(-box_length, box_length)
@@ -45,18 +46,21 @@ while total_polarity != 0:
 
 # Time step, simulation length
 dt = 0.1
-t_max = 10
-# Initialisers for fieldline calcs
+t_max = 5
+IPs = 10 # Number of initial field line IPs
+
+"""Initialisers for fieldline calcs"""
 C = 1 # Constant allows for flipping of B field for negative pols.
-t_vals = np.arange(0, 1, dt)
-# Generate random angles
-angles = np.arange(0,np.pi/2, 50) # From (0,pi/2) for the altitude, from (0,2pi) for azimuthal (eventually)
-# Define radius
-radii = 0.1 
-# Convert polar coordinates to cartesian coordinates
-Ix0 = radii * np.cos(angles)
-Iy0 = radii * np.sin(angles)
-Ixy = np.column_stack((Ix0, Iy0)) # Initial field line starting positions.
+t_vals = np.linspace(0, t_max, 10) # This refers to the timesteps/arclengths that will be integrated over in odeint
+""" Random points on sphere, following the algorithm as per Wolfram.
+We follow the final algorithm on the page - pick a random normal variable for each x, y and z and then normalise to r.
+"""
+def sample_spherical(npoints, ndim=3):
+    vec = np.random.randn(ndim, npoints)
+    vec /= np.linalg.norm(vec, axis=0)
+    return 0.1*vec
+#0.1 for radius
+Ix0, Iy0, Iz0 = sample_spherical(IPs)
 final_particle = np.zeros(n_particles)
 
 # Create a figure and axis object for the plot
@@ -121,39 +125,42 @@ def update(t,x,y,polarity):
     THE LOGIC NEEDS TO BE SWAPPED, FIELD LINE IPS AFTER THE PARTICLE. MAYBE NOT, CHECK THIS.
     """
     def field_line(M, t, C):
-        p, q = M
-        dBxds, dByds = 0, 0
+        p, q, z = M
+        dBxds, dByds, dBzds = 0, 0, 0
         for i in range(n_particles): # Contribution to B from each particle
             norm = np.sqrt((p-x[i])**2 + (q-y[i])**2)
             dBxds += ((p-x[i])/norm) * polarity[i]
             dByds += ((q-y[i])/norm) * polarity[i]
-        dBds = [C * dBxds, C * dByds]
+            dBzds += 0 # Since particles do not move on the z plane, the z contribution will always be zero.
+        dBds = [C * dBxds, C * dByds, C * dBzds] # Constant to confirm right direction of B field solving
         return dBds
-    i = 3 # Test particle
-    for j in range(len(Ixy)):
-        B0 = (Ix0[j]+x[i], Iy0[j]+y[i])
-        if polarity[i] >= 0:
-            sol = odeint(field_line, B0, t_vals, args=(C,))
-        else:
-            sol = odeint(field_line, B0, t_vals, args=(-C,))
-        end = np.array((sol[0,-1], sol[1,-1]))
-        for k in range(n_particles):
-            pos = np.array((x[k],y[k]))
-            print("At this point in time, particle 3 is at", (x[3],y[3]))
-            print("Before evolution, our field line IP {} started at {}".format(j, B0))
-            print("After evolution, the field line ended up at".format(end))
-            print("Particle {} is at {}.".format(k, pos))
-            if np.linalg.norm(pos - end) <= 1:
-                final_particle[k] += 1
-                print("Field line that started at {} and finished at {} was close enough to particle {} which was at {}".format(B0, end, k, pos))
-        print("NEW J")
+    I = 0 # Test particle
+    if np.linalg.norm((x[I],y[I])) < box_length: # i.e no need to bother checking if not in a suitable length.
+        for k in range(1,n_particles): # Do not want to include 0. How can we have this so that it checks for every particle except the chosen test particle?
+            pos = (x[k], y[k])
+            for j in range(IPs):
+                B0 = (Ix0[j]+x[I], Iy0[j]+y[I], Iz0[j])
+                if polarity[I] >= 0:
+                    sol = odeint(field_line, B0, t_vals , args=(C,))
+                else:
+                    sol = odeint(field_line, B0, t_vals, args=(-C,))
+                end = sol[j]
+                end = end[0:2] # Only need x and y coords
+                if np.linalg.norm(end-pos) < interaction_radius: # Seems reasonable
+                    final_particle[k] += 1
+                    print("Added to {} !".format(k))
+    results = [final_particle[a]/np.sum(final_particle) for a in range(n_particles)]
     print(final_particle)
+    print(results)
     axis.set_xlim(-box_length, box_length) # Ensures the animation looks more natural.
     axis.set_ylim(-box_length, box_length)
 
 # update(1, x, y, polarity)
 # Create the animation object
 anim = FuncAnimation(fig, update, frames=num_of_frames, fargs=(x,y,polarity), interval=10, repeat = False) #Code doesn't seem to want to run if I put repeat false/
+print("Terminated!")
+
+
 #anim.save('myanimation.gif') 
 # Currently the animation object does not terminate on its own.
 # Show the plot
--- test.py
+++ test.py
@@ -13,10 +13,7 @@ class magnetic_particle:
         self.vy = np.zeros(n_particles)
         self.polarity = np.zeros(n_particles) # Polarity encodes the charge too.
         self.colors = np.zeros(n_particles, dtype=str)
-    
-    # Set initial positions, and polarities of particles
-
-    def initial_charges(self):
+        "Modify charges so that they are randomised "
         for i in range(n_particles): # Initialise IPs
             self.x[i] = np.random.uniform(-box_length, box_length)
             self.y[i] = np.random.uniform(-box_length, box_length)
@@ -26,11 +23,67 @@ class magnetic_particle:
         # Check to ensure total pol is 0
         while total_polarity != 0:
             # Select polarity value that is not in the polarity array
-            new_polarity = np.random.choice([p for p in polarity_values if p not in polarity])
+            new_polarity = np.random.choice([p for p in polarity_values if p not in self.polarity])
             # Choose random particle to add or subtract the new polarity value
             random_index = np.random.randint(0, n_particles)
             self.polarity[random_index] += new_polarity
             total_polarity = np.sum(self.polarity)
+        self.final_particle = np.zeros(n_particles)
+        # Create a figure and axis object for the plot
+        self.fig, self.axis = plt.subplots()
+        self.num_of_frames = int(t_max/dt)
+        self.interaction_radius = 0.5
+
+    def update(self):
+        self.axis.clear()
+        """Initialisers for B-field"""
+        Bx, By = 0, 0
+        xf = np.arange(-box_length,box_length,0.25)
+        yf = np.arange(-box_length,box_length,0.25)
+        # Meshgrid
+        X, Y = np.meshgrid(xf,yf)
+
+        """Main for loop that checks for particle interactions, updates velocity based on a normal random walk, implements the resulting displacement, calculates B-field
+        etc. """
+        for i in range(n_particles):
+            for j in range(i+1, n_particles):
+                dist = np.sqrt((self.x[i]-self.x[j])**2+(self.y[i]-self.y[j])**2) 
+                if dist <= interaction_radius:
+                    # print('Interaction between {} and {}'.format(i, j))
+                    self.polarity [i] += self.polarity[j] # i.e. particle i adopts the polarity of J
+                    self.polarity [j] = 0 # The particle that has its polarity adopted is now no longer of any polarity.
+            if self.polarity [i] == 0:
+                self.x[i], self.y[i] = 100*box_length, 100*box_length # This will remove the particle from physical view and no impact on magnetic field calculations.
+                # print("particle {} is dead".format(i))
+            # Update the colour of the polarity
+            self.colors[i] = 'r' if self.polarity[i] >= 0 else 'b'
+            """Update Velocities based on a normal random walk"""
+            self.vx[i] = np.random.normal(loc=0, scale=velocity_scale) # Normal distribution centred around mean=loc and scaled by velocity_scale
+            self.vy[i] = np.random.normal(loc=0, scale=velocity_scale)
+            """Update particle positions, and implement the periodic boundary conditions."""
+            norm = np.linalg.norm((self.x[i],self.y[i]))
+            if norm < 3*box_length:
+                self.x[i] += self.vx[i] * dt
+                self.y[i] += self.vy[i] * dt
+                if self.x[i] > box_length:
+                    self.x[i] += -2*box_length
+                if self.x[i] < -box_length:
+                    self.x[i] += 2*box_length
+                if self.y[i] > box_length:
+                    self.y[i] += -2*box_length
+                if self.y[i] < -box_length:
+                    self.y[i] += 2*box_length
+            """Calculate B-field, and add visual tags for each particle"""
+            denom = np.sqrt((X-self.x[i])**2 + (Y-self.y[i])**2)
+            Bx += ((X-self.x[i])/denom) * self.polarity[i]
+            By += ((Y-self.y[i])/denom) * self.polarity[i]
+            self.axis.text(self.x[i], self.y[i], f"{i, self.polarity[i]}") # Show index and charge of particle alongside.
+        self.axis.scatter(self.x, self.y, c=self.colors, s=np.abs(self.polarity)*100)
+        # Streamline plot
+        self.axis.streamplot(X,Y,Bx,By, density=1.4, linewidth=None, color='#A23BEC')
+        self.axis.set_xlim(-box_length, box_length) # Ensures the animation looks more natural.
+        self.axis.set_ylim(-box_length, box_length)
+
 
 if __name__ == '__main__':
     # Constants
@@ -38,11 +91,24 @@ if __name__ == '__main__':
     velocity_scale = 3 # Determines the magnitude of each random walk
     box_length = 5
     n_points  = 100
-    polarity_values = np.arange(-10,10,1) # Possible polarity values. This allows us to randomise polarities such that they may sum to zero.
-    
+    polarity_values = np.arange(-10,10,1) # Possible polarity values. Does not exclude zero.
+    # Time step, simulation length
+    dt = 0.1
+    t_max = 10
+    num_of_frames = int(t_max/dt)
 
+    IPs = 10 # Number of initial field line IPs
+    interaction_radius = 0.5
+    # Initialisers for fieldline calcs
+    C = 1 # Constant allows for flipping of B field for negative pols.
+    t_vals = np.arange(0, t_max, dt)
+    # Generate random angles
+    angles = np.linspace(0, np.pi/2, IPs) # From (0,pi/2) for the altitude, from (0,2pi) for azimuthal (eventually)
+    # Define radius
+    radii = 0.1
+    # Convert polar coordinates to cartesian coordinates
+    Ix0 = radii * np.cos(angles)
+    Iy0 = radii * np.sin(angles)
     test_object = magnetic_particle()
-    print(test_object.polarity)
-    """
-    Call object/update fn
-    """
+    anim = FuncAnimation(test_object.fig, test_object.update, frames=num_of_frames, interval=10, repeat = False)
+    plt.show()
